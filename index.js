84be02b92df2fafd3cd3f43564649c23acaf49f2
Michelle David <rachaelholy@gmail.com>
2022-12-17 23:57:38 +0100

    fetch and get crypto name and image

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+//fetch and change window image and author name 
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
    .then(res => {
      if(!res.ok){
@@ -7,6 +7,15 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
      return res.json()
    })
    .then(data => {
+    console.log(data)
     document.body.style.backgroundImage = `url(${data.urls.regular})`
     document.getElementById('author').textContent = `Author: ${data.user.name}`
    })
+   .catch(Error => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+    )`
+    document.getElementById('author').textContent = 'Author: Dodi Achmad'
+
+   })
+
+//fetch and change crypto name and image
