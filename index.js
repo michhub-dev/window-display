13892f9f9097823ce608e2d9422010add50117ab
Michelle David <rachaelholy@gmail.com>
2022-12-17 23:29:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+   .then(res => {
+     if(!res.ok){
+        throw Error('Something funny went wrong')
+     }
+     return res.json()
+   })
+   .then(data => {
+    document.body.style.backgroundImage = `url(${data.urls.regular})`
+   })
